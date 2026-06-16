2614e79af06b856577c9dc77a4e4d8dfb19081e3
rec <35214@qq.com>
2026-06-16 18:38:49 +0800

    feat: 自动化初始化 services 业务层结构与调度接口定义

---
+++ charging_system/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChargingSystemConfig(AppConfig):
+    name = 'charging_system'
---
+++ charging_system/services/dispatch_service.py
@@ -0,0 +1,7 @@
+# 组长 负责 
+def priority_schedule(): pass 
+def time_slice_schedule(): pass 
+# D 负责 
+def time_order_schedule(): pass 
+# B 负责 
+def handle_pile_fault(fault_pile_id): pass
